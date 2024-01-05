d7dc92a4503c50c8498e9bd24fa29f352d32c868
Niteesh <yadlapalliniteesh@gmail.com>
2024-01-05 23:34:43 +0530

    Added Log and some progress in gameboard

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,18 +1,18 @@
 
-const initial_board = [
+const INITIAL_BOARD = [
     [null,null,null],
     [null,null,null],
     [null,null,null]
 ]
 
 export default function GameBoard(){
-
+    let initial_board = INITIAL_BOARD;
     return(
         <ol id="game-board">
             {initial_board.map((row,rowIndex) => <li key={rowIndex}>
                 <ol>
                     {row.map((col,colIndex) => <li key={colIndex}>
-                        <button>o</button>
+                        <button></button>
                     </li>)}
                 </ol>
             </li> )}
